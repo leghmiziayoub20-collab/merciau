9337820d6c71a39f0f97ffcf6aefaf531aa3b40a
reseaux company <leghmiziayoub20@gmail.com>
2026-05-13 02:14:54 +0100

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,28 +1,28 @@
-function sendEmail()  {
+function sendEmail() {
     const Data = {
-        to_email : "acrb371@gmail.com",
-        from_name: document.getElementById("name").value,
-        from_email : "acrb371@gmail.com",
-        subject : "support",
-        message : `
-        
-        name : ${document.getElementById("name").value}
-        email : ${document.getElementById("email").value}
-        message : ${document.getElementById("message").value}
-        
-        `,
-
+        to_email: "acrb371@gmail.com",
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: "support",
+        message: `
+name: ${document.getElementById("name").value}
+email: ${document.getElementById("email").value}
+message: ${document.getElementById("message").value}
+`
     };
-    emailjs,send("service_icpvtwp" , "template_cfyvysm", Data).then((response) => {
-        alert('success');
-    }).catch((error) => {
-        alert(+error);
 
-    });
+    emailjs.send("service_icpvtwp", "template_cfyvysm", Data)
+        .then(() => {
+            alert("sent successfully");
+        })
+        .catch((error) => {
+            alert(error.text || error.message);
+        });
 }
 
-
-
-document.getElementById("btn").addEventListener("click", () => {
+document.querySelector(".form-box").addEventListener("submit", (e) => {
+    e.preventDefault(); // 👈 هادي هي الحل
     sendEmail();
-})
+});
+
+
